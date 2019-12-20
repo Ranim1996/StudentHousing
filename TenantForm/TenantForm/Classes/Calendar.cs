1b7bf620120a28873e855891934eb8e02ee417f1
Zdravko Zdravkov <434121@student.fontys.nl>
2019-12-20 17:52:06 +0200

    asd

--- TenantForm/TenantForm/Classes/Calendar.cs
+++ TenantForm/TenantForm/Classes/Calendar.cs
@@ -12,12 +12,18 @@ namespace TenantForm.Classes
     public class Calendar
     {
         //list with dates and thir info
-        public List<FlowLayoutPanel> listFlDay = new List<FlowLayoutPanel>();
+        public List<FlowLayoutPanel> listFlDay { get; private set; }
 
         //current date
         private DateTime currentDate = DateTime.Today;
 
+        public Calendar()
+        {
+            listFlDay = new List<FlowLayoutPanel>();
 
+        }
+
+        // -- functions --
         private int GetFirstDayOfWeekOfCurrentDate()
         {
             DateTime firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
@@ -29,17 +35,35 @@ namespace TenantForm.Classes
             return firstDayOfCurrentDate.AddMonths(1).AddDays(-1).Day;
         }
 
-        private void PrevMonth()
+        // -- methods --
+        //used in the buttons
+        public void PrevMonth()
         {
             currentDate = currentDate.AddMonths(-1);
             DisplayCurrentDate();
         }
-        private void NextMonth()
+        public void NextMonth()
         {
             currentDate = currentDate.AddMonths(1);
             DisplayCurrentDate();
         }
-        private void GenerateDayPanel(int totalDays,FlowLayoutPanel flDays)
+        public void Today()
+        {
+            currentDate = DateTime.Today;
+            DisplayCurrentDate();
+        }
+        public void DisplayCurrentDate()
+        {
+            //lblMonthAndYear.Text = currentDate.ToString("MMMM, yyyy");
+            int firstDayAtFlNumber = GetFirstDayOfWeekOfCurrentDate();
+            int totalDay = GetTotalDaysOfCurrentDate();
+            AddLabelDayToFlDay(firstDayAtFlNumber, totalDay);
+
+            //when the database will work with this form, call the function below
+            // AddAppointmentToFlDay(firstDayAtFlNumber);
+        }
+        // -- MAIN methods --
+        public void GenerateDayPanel(int totalDays,FlowLayoutPanel flDays)
         {
             flDays.Controls.Clear();
             listFlDay.Clear();
@@ -55,13 +79,7 @@ namespace TenantForm.Classes
                 listFlDay.Add(fl);
             }
         }
-        private void Today()
-        {
-            currentDate = DateTime.Today;
-            DisplayCurrentDate();
-        }
-
-        private void AddLabelDayToFlDay(int startDayAtFlNumber, int totalDaysInMonth)
+        public void AddLabelDayToFlDay(int startDayAtFlNumber, int totalDaysInMonth)
         {
             foreach (FlowLayoutPanel fl in listFlDay)
             {
@@ -89,17 +107,50 @@ namespace TenantForm.Classes
 
             }
         }
-        private void DisplayCurrentDate()
+        
+
+        /*
+        //function for adding the event from the database to the specific day
+        private void AddAppointmentToFlDay(int startDayAtFlNumber)
         {
-            //lblMonthAndYear.Text = currentDate.ToString("MMMM, yyyy");
-            int firstDayAtFlNumber = GetFirstDayOfWeekOfCurrentDate();
-            int totalDay = GetTotalDaysOfCurrentDate();
-            AddLabelDayToFlDay(firstDayAtFlNumber, totalDay);
+            //-------
 
-            //when the database will work with this form, call the function below
-            // AddAppointmentToFlDay(firstDayAtFlNumber);
+            //the date of today is stored in the startDate variable and after that, with the sql variable it searches in the database
+            //for the startDate variable 
+            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
+            String sql = $"SELECT * FROM ... WHERE ... #{startDate.ToShortDateString()}# ";
+
+            //---------
+            //the connection with the dataBase
+            // the function from the database form is called
+
+             DataTable dt = QueryAsDataTable(sql);
+
+
+            //Display the events in the right day 
+            foreach(DataRow row in dt.Rows)
+            {
+                DateTime appDay = DateTime.Parse(row("AppDate"));
+                LinkLabel link = new LinkLabel();
+                link.Name = $"link{row("ID")}";
+                link.Text = row("Task");
+                listFlDay[ (appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
+            }
         }
+        */
 
+        // -------- Function which has to be used in the database form ------//
+        /*
+         * 
+         * public DataTable QueryAsDataTable(String sql)
+         * {
+         *      OleDbDataAdapter(sql, cn)  da;
+         *      DataSet ds;
+         *      da.Fill(ds, "result");
+         *      return ds.Tables("result");    
+         * }
+         */
 
+      
     }
 }
--- TenantForm/TenantForm/Form1.cs
+++ TenantForm/TenantForm/Form1.cs
@@ -14,188 +14,48 @@ namespace TenantForm
 {
     public partial class Form1 : Form
     {
-        private List<FlowLayoutPanel> listFlDay = new List<FlowLayoutPanel>();
-        
-        private DateTime currentDate = DateTime.Today;
-        //private List<Person> ppl;
-
-        private int hours = 0;
-        private int minutes = 0;
-
         //the list with announcemets is in this instance
         Announcements ann = new Announcements();
+        //holds the calendar
+        Calendar cal = new Calendar();
 
+        //variables for time managment
+        private int hours = 0;
+        private int minutes = 0;
 
         public Form1()
         {
             InitializeComponent();
-            //test ---> WORKING
-            //DataBasePlayground db = new DataBasePlayground();
-            //ppl = db.GetAllPeople();
-            //MessageBox.Show(ppl[0].u_Name);
-            //DateTime appDay = DateTime.Parse(row("AppDate"));
-            //LinkLabel link = new LinkLabel();
-            //DataRow
-            // ---> UNNCECESSARY 
-            //LogIn ls = new LogIn();
-            //ls.Show();
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            GenerateDayPanel(42);
-            DisplayCurrentDate();
+            // --- Calendar Tab ---
+            cal.GenerateDayPanel(42,flDays);
+            cal.DisplayCurrentDate();
             tbDateRequest.Text = dateTimePicker1.Value.ToString("dd/MM/yyyy");
 
             // --- Announcements Tab ---
-            // when the data base is ready
+            // --- READY 
             //ann.GetAllAnnouncements();
         }
-        
-        /*
-        //function for adding the event from the database to the specific day
-        private void AddAppointmentToFlDay(int startDayAtFlNumber)
-        {
-            //-------
-
-            //the date of today is stored in the startDate variable and after that, with the sql variable it searches in the database
-            //for the startDate variable 
-            DateTime startDate = new DateTime(currentDate.Year, currentDate.Month, 1);
-            String sql = $"SELECT * FROM ... WHERE ... #{startDate.ToShortDateString()}# ";
-
-            //---------
-            //the connection with the dataBase
-            // the function from the database form is called
-
-             DataTable dt = QueryAsDataTable(sql);
-
-
-            //Display the events in the right day 
-            foreach(DataRow row in dt.Rows)
-            {
-                DateTime appDay = DateTime.Parse(row("AppDate"));
-                LinkLabel link = new LinkLabel();
-                link.Name = $"link{row("ID")}";
-                link.Text = row("Task");
-                listFlDay[ (appDay.Day - 1) + (startDayAtFlNumber - 1)].Controls.Add(link);
-            }
-        }
-        */
-
-        // -------- Function which has to be used in the database form ------//
-        /*
-         * 
-         * public DataTable QueryAsDataTable(String sql)
-         * {
-         *      OleDbDataAdapter(sql, cn)  da;
-         *      DataSet ds;
-         *      da.Fill(ds, "result");
-         *      return ds.Tables("result");    
-         * }
-         */
-
-        private int GetFirstDayOfWeekOfCurrentDate()
-        {
-            DateTime firstDayOfMonth = new DateTime(currentDate.Year, currentDate.Month, 1);
-            return Convert.ToInt32(firstDayOfMonth.DayOfWeek + 1);
-        }
-
-        private int GetTotalDaysOfCurrentDate()
-        {
-            DateTime firstDayOfCurrentDate = new DateTime(currentDate.Year, currentDate.Month, 1);
-            return firstDayOfCurrentDate.AddMonths(1).AddDays(-1).Day;
-        }
-        
-        private void DisplayCurrentDate()
-        {
-            lblMonthAndYear.Text = currentDate.ToString("MMMM, yyyy");
-            int firstDayAtFlNumber = GetFirstDayOfWeekOfCurrentDate();
-            int totalDay = GetTotalDaysOfCurrentDate();
-            AddLabelDayToFlDay(firstDayAtFlNumber, totalDay);
-
-            //when the database will work with this form, call the function below
-            // AddAppointmentToFlDay(firstDayAtFlNumber);
-        }
-
-        private void PrevMonth()
-        {
-            currentDate = currentDate.AddMonths(-1);
-            DisplayCurrentDate();
-        }
-        private void NextMonth()
-        {
-            currentDate = currentDate.AddMonths(1);
-            DisplayCurrentDate();
-        }
-
-        private void GenerateDayPanel(int totalDays)
-        {
-            flDays.Controls.Clear();
-            listFlDay.Clear();
-            for(int i = 1; i <= totalDays; i++)
-            {
-                FlowLayoutPanel fl = new FlowLayoutPanel();
-                fl.Name = $"flDay{i}";
-                fl.Size = new Size(130, 90);
-                fl.BackColor = Color.White;
-                fl.BorderStyle = BorderStyle.FixedSingle;
-                fl.AutoScroll = true;
-                flDays.Controls.Add(fl);
-                listFlDay.Add(fl);
-            }
-        }
-
-        private void Today()
-        {
-            currentDate = DateTime.Today;
-            DisplayCurrentDate();
-        }
-
-        private void AddLabelDayToFlDay(int startDayAtFlNumber, int totalDaysInMonth)
-        {
-            foreach(FlowLayoutPanel fl in listFlDay)
-            {
-                fl.Controls.Clear();
-                fl.Tag = 0;
-                fl.BackColor = Color.White;
-            }
-            for(int i = 1; i <= totalDaysInMonth; i++)
-            {
-                Label lbl = new Label();
-                lbl.Name = $"lblDay{i}";
-                lbl.AutoSize = false;
-                lbl.TextAlign = ContentAlignment.MiddleRight;
-                lbl.Size = new Size(120, 23);
-                lbl.Text = i.ToString();
-                lbl.Font = new Font("Arial", 10);
-                listFlDay[(i - 1) + (startDayAtFlNumber - 1)].Tag = i;
-                listFlDay[ (i - 1) + (startDayAtFlNumber - 1) ].Controls.Add(lbl);
-
-                //change the color of today
-                if(new DateTime(currentDate.Year, currentDate.Month, i) == DateTime.Today)
-                {
-                    listFlDay[(i - 1) + (startDayAtFlNumber - 1)].BackColor = Color.Bisque;
-                }
-
-            }
-        }
-
+          
+        // --- Calendar Tab ---
         private void btnPrevMonth_Click(object sender, EventArgs e)
         {
-            PrevMonth();
+            cal.PrevMonth();
         }
 
         private void btnNextMonth_Click(object sender, EventArgs e)
         {
-            NextMonth();
+            cal.NextMonth();
         }
 
         private void btnToday_Click(object sender, EventArgs e)
         {
-            Today();
+            cal.Today();
         }
-
+        //-------------------------------------------------------------------------------------------
 
 
         // --- Make a Request Tab ---
